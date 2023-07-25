fc9fe4afe8c3112db2ab6111decb446fbf6de4a5
H1Rka <140366687+H1Rka@users.noreply.github.com>
2023-07-25 14:55:35 +0700

    Create aritmatika.js

---
+++ aritmatika.js
@@ -0,0 +1,14 @@
+let bil1=8;
+      bil2=7;
+//operasi aritmatika
+  let pertambahan=bil1+bil2;
+  let pengurangan=bil1-bil2;
+  let perkalian=bil1*bil2;
+  let pembagian=bil1/bil2;
+  let moduls=bil1%bil2;
+  
+  document.write("Hasil Pertambahan"+pertambahan+"<br>");
+  document.write("Hasil Pengurangan"+pengurangan+"<br>");
+  document.write("Hasil Perkalian"+perkalian+"<br>");
+  document.write("Hasil Pembagian"+pembagian+"<br>");
+  document.write("Hasil Moduls"+moduls+"<br>");
